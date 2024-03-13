8818896050b7862ab2fff071c7955f1e10973e3e
Abhitachi <abhishekshetter1999@gmail.com>
2024-03-13 23:41:30 +0530

    finaal commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GroceryForm from "./components/GroceryForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <GroceryForm />
+    </div>
+  );
+}
